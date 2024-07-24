e0851fb7efeaf1f6f58ef5b948c19e421f138129
Francesco Passanante <francescopassanante@gmail.com>
2024-07-24 14:25:35 +0200

    feat: hide scripts

--- src/views/dashboard.blade.php
+++ src/views/dashboard.blade.php
@@ -475,7 +475,7 @@
 
 @section('page-style')
     <!-- Page -->
-    <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/cards-advance.css') }}">
+    {{-- <link rel="stylesheet" href="{{ asset('assets/vendor/css/pages/cards-advance.css') }}"> --}}
 @endsection
 
 @section('vendor-script')
@@ -483,12 +483,9 @@
     <script src="{{ asset('assets/vendor/libs/toastr/toastr.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/apex-charts/apexcharts.js') }}"></script>
     <script src="{{ asset('assets/vendor/libs/datatables-bs5/datatables-bootstrap5.js') }}"></script>
-    {{-- <script src="{{ asset('assets/vendor/libs/datatables-responsive-bs5/responsive.bootstrap5.js') }}"></script>
-    <script src="{{ asset('assets/vendor/libs/datatables-checkboxes-jquery/datatables.checkboxes.js') }}"></script> --}}
 @endsection
 
 @section('page-script')
-    {{-- <script src="{{ asset('assets/js/dashboards-analytics.js') }}"></script> --}}
     <script>
         const initEventsPayments = () => {
             common_request.post('stats/events-payments')
